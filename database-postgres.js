161dc9e3ef2055d401baf98584d6e227562a5031
Breno Coelho <coelhobreno21@gmail.com>
2024-03-10 01:06:55 -0300

    update 2.0

--- database-postgres.js
+++ database-postgres.js
@@ -8,10 +8,20 @@ export class DataBaseMemory {
     async listProducts(search) {
         
         if(search){
-            return await sql`select * from products where title ilike ${'%'+search+'%'}`
+
+            const searchTerms = search.split(' ')
+
+            //É preciso utilizar o sql``. Se você passar uma string pura (sem usar a função sql), o executor SQL pode interpretá-la como uma consulta direta sem tratamento especial para parâmetros. Isso pode resultar em um erro, especialmente se a string contiver caracteres especiais que precisam ser escapados, ou se a consulta não estiver de acordo com a sintaxe esperada pelo banco de dados.
+            const query = sql`
+            SELECT * FROM products
+            WHERE description ILIKE ANY(${searchTerms.map(term => '%' + term + '%')})
+            `;
+
+            return await sql`${query}`
+
         }
 
-        return await sql`select * from products`
+        return await sql`SELECT * FROM products`
 
     }
 
@@ -19,9 +29,9 @@ export class DataBaseMemory {
 
         const productId = randomUUID()
 
-        const { title, description } = product
+        const { title, description, image, price } = product
 
-        await sql`insert into products (id, title, description) values (${productId}, ${title}, ${description})`
+        await sql`insert into products (id, title, description, image, price) values (${productId}, ${title}, ${description}, ${image}, ${price})`
 
     }
 
@@ -30,21 +40,17 @@ export class DataBaseMemory {
     }
 
     async updateProduct(id, product){
-        const { title, description } = product
+        const { title, description, image, price } = product
 
         await sql`UPDATE products 
-            set title = ${title}, description = ${description}
+            set title = ${title}, description = ${description}, image = ${image}, price = ${price}
             where id = ${id}
         `
     }
 
-    //CARTS
-    async listCarts(search) {
-        
-        return await sql`select * from products p 
-            inner join carts on p`
 
-    }
+
+
 
     //USERS
     async listUser(email) {
@@ -55,15 +61,105 @@ export class DataBaseMemory {
     }
 
     async createUser(user) {
-        const userID = randomUUID()
-        const { email, password, name, surname } = user
+        const { name, surname, email, password } = user
 
-        await sql`insert into users (
-            id, email, password, name, surname
-        ) VALUES (
-            ${userID}, ${email}, ${password}, ${name}, ${surname}
-        );`
-        
+        const userId = randomUUID()
+
+        try {
+
+            await sql`insert into users (
+                    id, name, surname, email, password
+                ) VALUES (
+                    ${userId}, ${name}, ${surname}, ${email}, ${password}
+                );`
+
+            await this.createCart(userId)
+
+        }catch(err){
+            return reply.status(201).send({
+                message: "Erro ao cadastrar usuário"
+            })
+        }
     }
 
+    async deleteUser(id) {
+        await sql`delete from users
+            where id = ${id}`
+    }
+
+
+
+
+
+    //CARTS
+    async createCart(userId) {
+        const id = randomUUID()
+        
+        await sql`insert into carts (id, user_id)
+            values (${id}, ${userId})`
+    }
+
+
+
+
+
+    //ITEMS_CART
+    async createItemCart({ productId, userId }){
+        const id = randomUUID()
+
+        const searchId = await sql`select c.id from users u 
+            left join carts c on u.id = c.user_id
+            where c.user_id = ${userId}`
+
+        const cartId = searchId[0].id
+        
+        const isRepeatedItem = await sql`
+            select qty from items_cart where product_id = ${productId} AND 
+            cart_id = ${cartId}
+        `
+        if(isRepeatedItem.length > 0){
+            const qty = isRepeatedItem[0].qty
+
+            return await sql`
+                UPDATE items_cart
+                set qty = ${qty + 1}
+                where product_id = ${productId} AND 
+                cart_id = ${cartId}
+            `
+        }
+
+        await sql`insert into items_cart (id, cart_id, product_id, qty)
+            values (${id}, ${cartId}, ${productId}, 1)`
+    }
+
+    async clearCart(){
+        await sql`delete from items_cart`
+    }
+
+    async deleteItemCart(itemId){
+        const item = await sql`select qty from items_cart where id = ${itemId}`
+        const qty = item[0].qty
+        if(qty > 1){
+            return await sql`UPDATE items_cart
+                set qty = ${qty - 1}
+                where id = ${itemId}`
+        }
+        
+        await sql`delete from items_cart where id = ${itemId}`
+    }
+
+    //PEGAR ITENS DO CARRINHO (inner join cart and items cart)
+    async listItensCart(userId){
+        const searchCartId = await sql`select c.id from carts c left join users u ON c.user_id = u.id where c.user_id = ${userId}`
+
+        const cartId = searchCartId[0].id
+
+        const cartItems = await sql`select ic.id as item_id, * from items_cart ic left join
+            products p ON ic.product_id = p.id left join
+            carts c ON ic.cart_id = c.id
+            where c.id = ${cartId}
+        `
+
+        return cartItems
+    }
 }
--- server.js
+++ server.js
@@ -16,12 +16,78 @@ server.addHook('onRequest', (req, reply, done) => {
 });
 
 //CONFIG CORS FROM /LOGIN
-server.options('/login', (req, reply) => {
+server.options('/users/login', (req, reply) => {
   reply.header('Access-Control-Allow-Origin', '*');
   reply.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
   reply.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   reply.send();
 });
+
+//CONFIG CORS FROM /REGISTER
+server.options('/users/register', (req, reply) => {
+  reply.header('Access-Control-Allow-Origin', '*');
+  reply.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  reply.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  reply.send();
+});
+
+//CONFIG CORS /CARTS/ITEMS
+// server.route({
+//   method: 'OPTIONS',
+//   url: '/carts/items',
+//   handler: (req, reply) => {
+//     reply
+//       .header('Access-Control-Allow-Origin', '*')
+//       .header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//       .header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//       .send();
+//   }
+// });
+
+server.options('/carts/items', (req, reply) => {
+  reply.header('Access-Control-Allow-Origin', '*');
+  reply.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  reply.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  reply.send();
+});
+
+server.options('/carts/items/:id', (req, reply) => {
+  reply.header('Access-Control-Allow-Origin', '*');
+  reply.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  reply.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  reply.send();
+});
+
+server.options('/verify-user', (req, reply) => {
+  reply.header('Access-Control-Allow-Origin', '*');
+  reply.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  reply.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  reply.send();
+});
+
+
+
+
+
+//VERIFY TOKEN
+server.post('/verify-user', (req, res) => {
+    try{
+        const token = req.body
+        
+        const decoded = jwt.verify(token, 'Verdaomago15@')
+        res.status(200).send({ valid: true, decoded });
+    }catch(err){
+        res.status(401).send({ valid: false, message: 'Token inválido' });
+    }
+})
+
+server.get('/verify-user', (req, res) => {
+    res.send('Backend is acessible!')
+})
+
+
+
+
 
 //PRODUCTS
 server.get('/products', async(request) => {
@@ -35,12 +101,7 @@ server.get('/products', async(request) => {
 server.post('/products', async (request, reply) => {
     const body = request.body
 
-    const { title, description } = body
-
-    await database.createProduct({
-        title,
-        description
-    })
+    await database.createProduct(body)
 
     return reply.status(201).send()
 })
@@ -63,20 +124,22 @@ server.delete('/products/:id', async (request, reply) => {
 })
 
 
+
+
+
 //USERS
-server.post('/register', async(request, reply) => {
+server.post('/users/register', async(request, reply) => {
     try {
-        const { email, password, name, surname } = request.body
+        const { name, surname, email, password } = request.body
 
         //Verificar se o email já está cadastrado
         const existingUser = await database.listUser(email)
         if(existingUser.length > 0){
-            
             return reply.status(400).send({
                 message: 'Este email já está cadastrado.'
             })
-            
         }
+        //Se o email não estiver cadastrado...
 
         //Gerando salt para usar na geração do hash (Ou seja, uma sequencia de caracteres aleatórios que é adicionado a senha antes de ser criptografado)
         const saltRounds = 10; //Definindo 10 caractecteres aleatórios
@@ -86,13 +149,12 @@ server.post('/register', async(request, reply) => {
         //Gerar o hash da senha utilizando o salt
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        //Se este email não estiver cadastrado, pode-se prosseguir
         //Criando usuário
         await database.createUser({
+            name,
+            surname,
             email, 
-            password: hashedPassword, 
-            name, 
-            surname
+            password: hashedPassword
         })
 
         return reply.status(201).send({
@@ -106,44 +168,127 @@ server.post('/register', async(request, reply) => {
     }
 })
 
-server.post('/login', async(request, reply) => {
-    
-    const { email, password } = request.body
+server.post('/users/login', async(request, reply) => {
+    try{
+        const { email, password } = request.body
 
-    //verificar se o usuário existe no banco
-    const user = await database.listUser(email)
-    if(!user){
-        return reply.status(401).send({
-            message: "Credenciais Inválidas"
+        //verificar se o usuário existe no banco
+        const user = await database.listUser(email)
+        if(!user){
+            return reply.status(401).send({
+                message: "Credenciais Inválidas. Verifique seu email."
+            })
+        }
+
+        //comparar a senha fornecida com a senha armazenada
+        const isPasswordValid = await bcrypt.compare(password, user[0].password)
+        if(!isPasswordValid){
+            return reply.status(401).send({
+                message: "Credenciais Inválidas. Verifique sua senha."
+            })
+        }
+        
+        //gerar token
+        const expiresIn = '7d'
+        const token = jwt.sign({ id: user[0].id, email }, 'Verdaomago15@', { expiresIn })
+
+        //retirando senha do usuário
+        const { password: authorize, ...rest } = user[0]
+
+        //retornar token e usuario
+        return reply.status(201).send({
+            user: rest,
+            token
         })
+    }catch(err){
+        return reply.status(500).send({ 
+            message: 'Erro interno do servidor autenticar o usuário' 
+        });
     }
-
-    //comparar a senha fornecida com a senha armazenada
-    const isPasswordValid = await bcrypt.compare(password, user[0].password)
-    if(!isPasswordValid) {
-        return reply.status(401).send({
-            message: "Credenciais Inválidas"
-        })
-    }
-    
-    //gerar token
-    const token = jwt.sign({ id: user.id, email }, 'Verdaomago15@')
-
-    //retornar token e usuario
-    return reply.status(201).send({ 
-        user,
-        token
-    })
 
 })
 
-//CARTS
-// server.post('/carts/:id', async(request, reply) => {
+//DELETE USER
+server.delete('/users/:id', async(request, reply) => {
+    const userId = request.params.id
+    if(!userId){
+        return reply.status(401).send({
+            message: "Usuário não identificado!"
+        })
+    }
 
-// })
+    await database.deleteUser(userId)
+
+    return reply.status(200).send({
+        message: "Usuário removido!"
+    })
+})
+
+
+
+
+
+//ITEMS_CART
+server.post('/carts/items', async(request, reply) => {
+    try{
+        const body = request.body
+
+        await database.createItemCart(body)
+
+        return reply.status(201).send({
+            message: "Item adicionado ao carrinho."
+        })
+    }catch(err){
+        return reply.status(401).send({
+            message: "O produto não foi adicionado ao carrinho."
+        })
+    }
+})
+
+server.delete('/carts/items', async(request, reply) => {
+    try{
+        await database.clearCart()
+
+        return reply.status(200).send({
+            message: "Itens removidos do carrinho."
+        })
+    }catch(err){
+        return reply.status(401).send({
+            message: "O carrinho não foi limpo."
+        })
+    }
+})
+
+//Itens de um carrinho específico
+server.get('/carts/items/:id', async(request, reply) => {
+    const userId = request.params.id
+
+    const itemsInCart = await database.listItensCart(userId)
+
+    return reply.status(201).send(itemsInCart)
+})
+
+//Deletar item
+server.delete('/carts/items/:id', async(request, reply) => {
+    try{
+
+        const itemId = request.params.id
+
+        await database.deleteItemCart(itemId)
+
+        return reply.status(200).send({
+            message: "Item removido do carrinho."
+        })
+
+    }catch(err){
+        return reply.status(401).send({
+            message: "O produto não foi removido do carrinho."
+        })
+    }
+})
+
 
 server.listen({
     host: "0.0.0.0",
     port: process.env.PORT ?? 3333
-
  })
